d8688cf3f83d82d641873d60629cb5e635d1924c
CarloPierotto <pierotto.carlo@gmail.com>
2025-09-22 23:10:16 +0200

    Version 1
    Punkte werden anhand von API generiert

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,35 @@
 // Karte erstellen und zentrieren
-    var map = L.map('map', {zoomControl: false}).setView([46.851712, 9.524212], 13);
+var map = L.map('map', {zoomControl: false}).setView([46.851712, 9.524212], 13);
 
 // OpenStreetMap Tiles laden
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-// Fester Kreismarker
-    var circle = L.circleMarker([46.85, 9.53], {
-         radius: 20,        // feste Grösse
-        color: 'red',      // Randfarbe
-        fillColor: 'red', // Füllfarbe
+//API Datenspeicher
+let ApiData = [];
+
+//Daten von der API holen und in ApiData speichern
+fetch("https://api.nextbike.net/maps/nextbike-live.json")
+  .then(res => res.json())
+  .then(data => {
+    ApiData = data.countries[126].cities[0].places.map(spot => ({
+        name: spot.name,
+        lat: spot.lat,
+        lng: spot.lng,
+        bikes: spot.bikes
+    })); 
+    console.log("fetchen hat funktioniert");
+
+    // Foreach Schleife, um Kreise zu erstellen
+    ApiData.forEach(place => {
+    L.circleMarker([place.lat, place.lng], {
+        radius: Math.max(place.bikes + 1),
+        color: "red",
+        fillColor: "red",
         fillOpacity: 0.6,
         opacity: 0.3
-    }).addTo(map);
-
-
-     var circle = L.circleMarker([46.86, 9.52], {
-         radius: 10,        // feste Grösse
-        color: 'red',      // Randfarbe
-        fillColor: 'red', // Füllfarbe
-        fillOpacity: 0.6,
-        opacity: 0.3
-    }).addTo(map);
+    })
+    .addTo(map)
+    })
+  })
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # IM-3_Mooinz-Checker
+---------------------------------------------
+Tim Eberhard
+Carlo Pierotto
+---------------------------------------------
+Mit dieser Website siehst du, wo in Chur wie viele MOOINZ Velos sind und wie viele normalerweise dort sind.
